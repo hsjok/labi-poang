0bb3b17a4237ce6d5c01f1d72be8a95930b73ae6
sjokvist henrik <sjokvist.henrik@bcg.com>
2023-12-02 19:24:17 -0500

    black and flake8

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
 from extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
@@ -9,17 +8,23 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     balance = db.Column(db.Integer, default=0)
-    transactions = db.relationship('Transaction', backref='user', lazy=True)
+    transactions = db.relationship("Transaction", backref="user", lazy=True)
     password_hash = db.Column(db.String(256))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "<User %r>" % self.username
 
     def add_points(self, points, description=""):
         old_balance = self.balance
         self.balance += points
         new_balance = self.balance
-        transaction = Transaction(user_id=self.id, old_balance=old_balance, change=points, new_balance=new_balance, description=description)
+        transaction = Transaction(
+            user_id=self.id,
+            old_balance=old_balance,
+            change=points,
+            new_balance=new_balance,
+            description=description,
+        )
         db.session.add(transaction)
 
     def subtract_points(self, points, description=""):
@@ -27,7 +32,13 @@ class User(db.Model):
             old_balance = self.balance
             self.balance -= points
             new_balance = self.balance
-            transaction = Transaction(user_id=self.id, old_balance=old_balance, change=-points, new_balance=new_balance, description=description)
+            transaction = Transaction(
+                user_id=self.id,
+                old_balance=old_balance,
+                change=-points,
+                new_balance=new_balance,
+                description=description,
+            )
             db.session.add(transaction)
             return True
         return False
@@ -41,7 +52,7 @@ class User(db.Model):
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     old_balance = db.Column(db.Integer, nullable=False)
     change = db.Column(db.Integer, nullable=False)
     new_balance = db.Column(db.Integer, nullable=False)
